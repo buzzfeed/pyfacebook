40dbd5fa834906e1654bf22522ddb2b2b5acaef7
Jeff <jeff.revesz@buzzfeed.com>
2013-06-27 16:23:18 -0400

    Added broadtargetingcategory endpoint

--- pyfacebook/models/adaccount.py
+++ pyfacebook/models/adaccount.py
@@ -1,22 +1,25 @@
 from pyfacebook.models import Model
 
-class AdAccount( Model ):
-  """
-  The AdAccount class represents the adaccount object in the Facebook Ads API:
-  https://developers.facebook.com/docs/reference/ads-api/adaccount/
 
-  """
-  attrs_to_copy = ( 'id',
-                    'account_id',
-                    'name',
-                    'account_status',
-                    'currency',
-                    'timezone_id',
-                    'timezone_name',
-                    'vat_status',
-                    'daily_spend_limit',
-                    'amount_spent',
-                    'adusers',
-                    'adgroups',
-                    'adstatistic'
-                  )
+class AdAccount(Model):
+
+    """
+    The AdAccount class represents the adaccount object in the Facebook Ads API:
+    https://developers.facebook.com/docs/reference/ads-api/adaccount/
+
+    """
+    attrs_to_copy = ('id',
+                     'account_id',
+                     'name',
+                     'account_status',
+                     'currency',
+                     'timezone_id',
+                     'timezone_name',
+                     'vat_status',
+                     'daily_spend_limit',
+                     'amount_spent',
+                     'adusers',
+                     'adgroups',
+                     'adstatistic',
+                     'broadtargetingcategories',
+                     )
---
+++ pyfacebook/models/broadtargetingcategory.py
@@ -0,0 +1,13 @@
+from pyfacebook.models import Model
+
+
+class BroadTargetingCategory(Model):
+
+    """
+    The BroadTargetingCategory class represents the broadtargetingcategory object in the Facebook Ads API:
+    https://developers.facebook.com/docs/reference/ads-api/broadtargetingcategory/
+    We extend the functionality in the documentation with custom calls for adaccounts
+
+    """
+
+    attrs_to_copy = ('id', 'adaccounts', 'role')
