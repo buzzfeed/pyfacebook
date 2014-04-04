2caaf4cdedbbfcdb6453869ba0e622d01eb35d61
Jeff <jeff.revesz@buzzfeed.com>
2014-04-04 12:02:25 -0400

    bumped to 1.1.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='pyfacebook',
-    version='1.1.5',
+    version='1.1.6',
     author='Jeff Revesz and Andrew Kelleher',
     author_email='jeff.revesz@buzzfeed.com',
     packages=find_packages(),
